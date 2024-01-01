c2330bfe377050bf3828be56da64343e70ae0d45
Prateek Chaurasia <prateek18801@gmail.com>
2024-01-01 18:40:05 +0530

    Add package controller

---
+++ controller/package.js
@@ -0,0 +1,19 @@
+const getPackages = (req, res, next) => {
+
+}
+const postPackages = (req, res, next) => {
+
+}
+const patchPackages = (req, res, next) => {
+
+}
+const deletePackages = (req, res, next) => {
+
+}
+
+module.exports = {
+    getPackages,
+    postPackages,
+    patchPackages,
+    deletePackages
+};
