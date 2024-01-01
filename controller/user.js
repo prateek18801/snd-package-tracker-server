5ff4f37bc66f83384faa72dca7cc2319f6876f31
Prateek Chaurasia <prateek18801@gmail.com>
2024-01-02 02:14:31 +0530

    Create post user controller

--- controller/user.js
+++ controller/user.js
@@ -1,19 +1,30 @@
 const User = require("../model/user");
 
 const getUsers = async (req, res, next) => {
-    
+
 }
 
 const postUsers = async (req, res, next) => {
-    
+    try {
+        // TODO - validate request body
+        const { password, __v, ...user } = (await new User(req.body).save()).toObject();
+        return res.status(201).set({
+            "location": `/v1/users/${user._id}`
+        }).json({
+            message: "user created",
+            data: user
+        });
+    } catch (err) {
+        next(err);
+    }
 }
 
-const patchUsers = (req, res, next) => {
+const patchUsers = async (req, res, next) => {
 
 }
 
 const deleteUsers = async (req, res, next) => {
-    
+
 }
 
 module.exports = {
