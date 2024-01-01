de251fd5d22aba31a409a6a30642ddf594f7d28e
Prateek Chaurasia <prateek18801@gmail.com>
2024-01-01 18:39:01 +0530

    Add user controllers

---
+++ controller/user.js
@@ -0,0 +1,24 @@
+const User = require("../model/user");
+
+const getUsers = async (req, res, next) => {
+    
+}
+
+const postUsers = async (req, res, next) => {
+    
+}
+
+const patchUsers = (req, res, next) => {
+
+}
+
+const deleteUsers = async (req, res, next) => {
+    
+}
+
+module.exports = {
+    getUsers,
+    postUsers,
+    patchUsers,
+    deleteUsers
+};
