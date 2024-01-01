2c45cda197cf99e07071711b785cea5cb56c6b7e
Prateek Chaurasia <prateek18801@gmail.com>
2024-01-02 02:15:28 +0530

    Create patch user controller

--- controller/user.js
+++ controller/user.js
@@ -20,7 +20,30 @@ const postUsers = async (req, res, next) => {
 }
 
 const patchUsers = async (req, res, next) => {
+    const id = req.params.id;
+    try {
+        // TODO - handle invalid ObjectId error
+        // TODO - validate request body
+        const user = await User.findByIdAndUpdate(id, req.body, { new: true })
+            .select({
+                __v: 0,
+                password: 0
+            })
+            .lean();
 
+        if (user) {
+            return res.status(200).json({
+                message: "user updated",
+                data: user
+            });
+        }
+
+        return res.status(400).json({
+            message: "user not found"
+        });
+    } catch (err) {
+        next(err);
+    }
 }
 
 const deleteUsers = async (req, res, next) => {
