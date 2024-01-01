fbd175caada36e3f113988eaa064685ebb765b97
Prateek Chaurasia <prateek18801@gmail.com>
2024-01-01 19:10:11 +0530

    Add user and package routes

---
+++ router/index.js
@@ -0,0 +1,29 @@
+const router = require("express").Router();
+
+const {
+    getUsers,
+    postUsers,
+    patchUsers,
+    deleteUsers
+} = require("../controller/user");
+
+const {
+    getPackages,
+    postPackages,
+    patchPackages,
+    deletePackages
+} = require("../controller/package");
+
+// user routes
+router.get("/v1/users", getUsers);
+router.post("/v1/users", postUsers);
+router.patch("/v1/users/:id", patchUsers);
+router.delete("/v1/users/:id", deleteUsers);
+
+// package routes
+router.get("/v1/packages", getPackages);
+router.post("/v1/packages", postPackages);
+router.patch("/v1/packages/:id", patchPackages);
+router.delete("/v1/packages/:id", deletePackages);
+
+module.exports = router;
