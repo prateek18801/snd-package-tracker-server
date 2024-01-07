f4bb5fb798b0caed6c4eedf1e27ea6f01a7a35be
Prateek Chaurasia <prateek18801@gmail.com>
2024-01-07 11:49:34 +0530

    Add login controller

---
+++ controller/auth.js
@@ -0,0 +1,33 @@
+import jwt from "jsonwebtoken";
+import User from "../model/user.js";
+
+const postLogin = async (req, res, next) => {
+    const { username, password } = req.body;
+    try {
+        const user = await User.findOne({ username });
+        if (user) {
+            if (await user.match(password)) {
+                const payload = {
+                    sub: user._id,
+                    role: user.role,
+                    name: user.name,
+                    username: user.username,
+                }
+                const token = jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: "1d" });
+                return res.status(200).json({
+                    token,
+                    user: payload
+                });
+            }
+        }
+        return res.status(401).json({
+            message: "invalid credentials"
+        });
+    } catch (err) {
+        next(err);
+    }
+}
+
+export {
+    postLogin
+};
