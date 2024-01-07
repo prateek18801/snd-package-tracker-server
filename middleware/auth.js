df97438f26259b2e8537284335ead3469b9d8e19
Prateek Chaurasia <prateek18801@gmail.com>
2024-01-07 18:26:58 +0530

    Update auth middleware to accept roles

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,12 +1,18 @@
 import jwt from "jsonwebtoken";
+import { isUnder } from "../utils/authorization.js";
 
-const auth = () => {
+const auth = (role) => {
     return (req, res, next) => {
         try {
             const token = req.headers?.authorization?.split(" ")[1];
             if (token) {
-                const {iat, exp, ...user} = jwt.verify(token, process.env.JWT_SECRET);
+                const { iat, exp, ...user } = jwt.verify(token, process.env.JWT_SECRET);
                 req.user = user;
+                if (role && isUnder(user.role, role)) {
+                    return res.status(403).json({
+                        message: "forbidden"
+                    });
+                }
                 return next();
             }
             return res.status(401).json({
