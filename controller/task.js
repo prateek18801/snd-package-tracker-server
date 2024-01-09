e7cf82a3d9e49be5f5b94e05acedd68356f772d2
Prateek Chaurasia <prateek18801@gmail.com>
2024-01-09 12:41:07 +0530

    Create task controller

---
+++ controller/task.js
@@ -0,0 +1,24 @@
+import Task from "../model/task";
+
+const getTasks = (req, res, next) => {
+
+}
+
+const postTasks = async (req, res, next) => {
+
+}
+
+const patchTasks = (req, res, next) => {
+
+}
+
+const deleteTasks = (req, res, next) => {
+
+}
+
+export {
+    getTasks,
+    postTasks,
+    patchTasks,
+    deleteTasks
+};
