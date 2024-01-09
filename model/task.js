6b2ffc1faaca2eb003243d3cb978f7c4ac0c8d06
Prateek Chaurasia <prateek18801@gmail.com>
2024-01-09 12:40:05 +0530

    Add task model

---
+++ model/task.js
@@ -0,0 +1,60 @@
+import { Schema, model } from "mongoose";
+import config from "../utils/config.js";
+
+const TaskSchema = new Schema({
+    task_id: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    type: {
+        type: String,
+        required: true,
+        enum: ["incoming", "outgoing"]
+    },
+    status: {
+        type: String,
+        default: "open",
+        enum: ["open", "closed"]
+    },
+    courier: {
+        type: String,
+        required: true,
+        enum: config.courier
+    },
+    channel: {
+        type: String,
+        required: true,
+        enum: config.channel
+    },
+    packages: [{
+        type: Schema.Types.ObjectId,
+        ref: "Package"
+    }],
+    created_by: {
+        type: Schema.Types.ObjectId,
+        required: true,
+        ref: "User"
+    },
+    updated_by: {
+        type: Schema.Types.ObjectId,
+        required: true,
+        ref: "User"
+    },
+    closed_at: Date
+}, {
+    timestamps: {
+        createdAt: "created_at",
+        updatedAt: "updated_at"
+    }
+});
+
+TaskSchema.pre("save", function (next) {
+    // check if status has been updated to closed
+    if (this.isModified("status") && this.status === "closed") {
+        this.closed_at = Date.now();
+    }
+    next();
+});
+
+export default model("Task", TaskSchema);
