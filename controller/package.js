1078b315578814d65fd1eff330d622f1c373f0a4
Prateek Chaurasia <prateek18801@gmail.com>
2024-01-10 13:46:23 +0530

    Implement taskid in package Fix pagination first_page index

--- controller/package.js
+++ controller/package.js
@@ -1,4 +1,5 @@
 import Package from "../model/package.js";
+import Task from "../model/task.js";
 import { isUnder, isOver } from "../utils/authorization.js";
 
 const getPackages = async (req, res, next) => {
@@ -7,9 +8,9 @@ const getPackages = async (req, res, next) => {
     try {
         if (id) {
             // TODO - handle invalid ObjectId error
-            const _package = await Package.findById(id).select({ __v: 0 }).lean();
+            const spackage = await Package.findById(id).select({ __v: 0 }).lean();
             return res.status(200).json({
-                data: _package
+                data: spackage
             });
         }
 
@@ -22,7 +23,7 @@ const getPackages = async (req, res, next) => {
 
         const page = req.query.page ? Math.max(+req.query.page, 1) : 1;
         const limit = req.query.limit ? Math.max(+req.query.limit, 1) : 1000;
-        const _package = await Package.find(filter)
+        const spackage = await Package.find(filter)
             .select({ __v: 0 })
             .skip((page - 1) * limit)
             .limit(limit)
@@ -36,7 +37,7 @@ const getPackages = async (req, res, next) => {
 
         return res.status(200).set({
             "x-total-count": total_count,
-            "link": `</v1/packages?page=${page}&per_page=${limit}>;rel="self",</v1/packages?page=${next_page}&per_page=${limit}>;rel="next",</v1/packages?page=${prev_page}&per_page=${limit}>;rel="previous",</v1/packages?page=0&per_page=${limit}>;rel="first",</v1/packages?page=${total_pages}&per_page=${limit}>;rel="last",`
+            "link": `</v1/packages?page=${page}&per_page=${limit}>;rel="self",</v1/packages?page=${next_page}&per_page=${limit}>;rel="next",</v1/packages?page=${prev_page}&per_page=${limit}>;rel="previous",</v1/packages?page=1&per_page=${limit}>;rel="first",</v1/packages?page=${total_pages}&per_page=${limit}>;rel="last",`
         }).json({
             metadata: {
                 page,
@@ -47,11 +48,11 @@ const getPackages = async (req, res, next) => {
                     self: `/v1/packages?page=${page}&per_page=${limit}`,
                     next: `/v1/packages?page=${next_page}&per_page=${limit}`,
                     prev: `/v1/packages?page=${prev_page}&per_page=${limit}`,
-                    first: `/v1/packages?page=0&per_page=${limit}`,
+                    first: `/v1/packages?page=1&per_page=${limit}`,
                     last: `/v1/packages?page=${total_pages}&per_page=${limit}`
                 }
             },
-            data: _package
+            data: spackage
         });
     } catch (err) {
         next(err);
@@ -70,11 +71,26 @@ const postPackages = async (req, res, next) => {
             // update timestamp only if role >= administrator
             timestamp: isOver(req.user.role, "executive") ? (req.body.timestamp || Date.now()) : Date.now(),
             executive: req.user.sub,
+            task: req.body.task_id,
             remarks: req.body.remarks
         }
     }
     try {
+        const task = await Task.findById(req.body.task_id);
+        if (!task) {
+            return res.status(400).json({
+                message: "task not found"
+            });
+        }
+        if (task.status === "closed") {
+            return res.status(400).json({
+                message: "task closed"
+            });
+        }
         const spackage = await new Package(data).save();
+        task.packages.push(req.body.task_id);
+        await task.save();
+
         return res.status(201).set({
             "location": `/v1/packages/${spackage._id}`
         }).json({
@@ -99,11 +115,12 @@ const patchPackages = async (req, res, next) => {
                 incoming: {
                     timestamp: Date.now(),
                     executive: req.user.sub,
+                    task: req.body.task_id,
                     remarks: req.body.remarks
                 }
             }
         } else {
-            if(isUnder(req.user.role, "administrator")) {
+            if (isUnder(req.user.role, "administrator")) {
                 return res.status(403).json({
                     message: "forbidden"
                 });
@@ -116,11 +133,12 @@ const patchPackages = async (req, res, next) => {
                 outgoing: {
                     timestamp: isOver(req.user.role, "executive") ? (req.body.timestamp || Date.now()) : Date.now(),
                     executive: req.user.sub,
+                    task: req.body.task_id,
                     remarks: req.body.remarks
                 }
-            }   
+            }
         }
-        const upackage = await Package.findOneAndUpdate({package_id: id}, update, { new: true })
+        const upackage = await Package.findOneAndUpdate({ package_id: id }, update, { new: true })
             .select({ __v: 0 })
             .lean();
         if (upackage) {
