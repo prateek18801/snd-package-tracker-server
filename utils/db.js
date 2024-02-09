1498b4c59578a635ff0b7b3ba4b3f92556749f6b
Prateek Chaurasia <prateek18801@gmail.com>
2024-02-10 00:10:49 +0530

    Minor code refactoring

--- utils/db.js
+++ utils/db.js
@@ -3,13 +3,16 @@ import mongoose from "mongoose";
 async function connect() {
     try {
         await mongoose.connect(process.env.DB_URI);
-        console.log("🟢 db connected");
+        console.log("✅ db connected");
     } catch (err) {
-        console.log("🔴 db connection failed", err.code);
+        console.log("❌ db connection failed", err.code);
     }
-
-    mongoose.connection.on("disconnected", () => console.log("🔴 db disconnected"));
-    mongoose.connection.on("reconnected", () => console.log("🟢 db reconnected"));
+    mongoose.connection.on("disconnected", () => {
+        console.log("❌ db disconnected")
+    });
+    mongoose.connection.on("reconnected", () => {
+        console.log("✅ db reconnected")
+    });
     const gracefulExit = async () => {
         await mongoose.connection.close();
         process.exit(0);
