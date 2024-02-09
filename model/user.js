3c908ab4c1fb7be02c8e28c5474a6cf28a2421d9
Prateek Chaurasia <prateek18801@gmail.com>
2024-02-09 14:23:06 +0530

    Update user admin role

--- model/user.js
+++ model/user.js
@@ -19,7 +19,7 @@ const UserSchema = new Schema({
     },
     role: {
         type: String,
-        enum: ["executive", "administrator", "manager", "root"],
+        enum: ["executive", "admin", "manager", "root"],
         default: "executive"
     },
     archived: {
