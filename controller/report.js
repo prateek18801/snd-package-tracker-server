b90885d827ce1f54af9e4d4cf3b5c3c5dcf746a6
Prateek Chaurasia <prateek18801@gmail.com>
2024-02-15 02:05:12 +0530

    Implement schema changes add task specific fields in task report

--- controller/report.js
+++ controller/report.js
@@ -6,44 +6,37 @@ import Task from "../model/task.js";
 import Package from "../model/package.js";
 
 const getPackageReport = async (req, res, next) => {
-    const { outgoing_gte, outgoing_lte, incoming_gte, incoming_lte } = req.query;
+    const { timestamp_gte, timestamp_lte } = req.query;
     try {
         const filter = {};
-        for (const key of ["channel", "courier", "status"]) {
+        for (const key of ["type", "channel", "courier"]) {
             if (req.query[key]) {
                 filter[key] = req.query[key];
             }
         }
 
-        if (outgoing_gte && outgoing_lte && new Date(outgoing_gte) < new Date(outgoing_lte)) {
-            filter["outgoing.timestamp"] = {
-                $gte: `${outgoing_gte}T00:00:00.000+05:30`,
-                $lte: `${outgoing_lte}T00:00:00.000+05:30`
+        if (timestamp_gte && timestamp_lte && new Date(timestamp_gte) < new Date(timestamp_lte)) {
+            filter["created_at"] = {
+                $gte: `${timestamp_gte}T00:00:00.000+05:30`,
+                $lte: `${timestamp_lte}T00:00:00.000+05:30`
             }
         }
 
-        if (incoming_gte && incoming_lte && new Date(incoming_gte) < new Date(incoming_lte)) {
-            filter["incoming.timestamp"] = {
-                $gte: `${incoming_gte}T00:00:00.000+05:30`,
-                $lte: `${incoming_lte}T00:00:00.000+05:30`
-            }
-        }
+        const packages = await Package.find(filter)
+            .populate("task", "task_id")
+            .populate("executive", "name username")
+            .lean();
 
-        const pkgs = await Package.find(filter).lean();
-
-        const report_data = pkgs.map(record => ({
+        const report_data = packages.map(record => ({
             package_id: record.package_id,
+            type: record.type,
             courier: record.courier,
             channel: record.channel,
-            status: record.status,
-            outgoing_ts: new Date(record.outgoing.timestamp).toLocaleString(),
-            outgoing_executive: record.outgoing.executive,
-            outgoing_task: record.outgoing.task,
-            outgoing_remarks: record.outgoing.remarks,
-            incoming_ts: (record.incoming?.timestamp && new Date(record.incoming.timestamp).toLocaleString()) || "",
-            incoming_executive: record.incoming?.executive || "",
-            incoming_task: record.incoming?.task || "",
-            incoming_remarks: record.incoming?.remarks || ""
+            status: record.cancelled ? "cancel" : record.type === "incoming" ? "return" : "dispatch",
+            executive: `${record.executive?.name} (${record.executive?.username})`,
+            task_id: record.task?.task_id,
+            remarks: record.remarks || "",
+            scanned_at: new Date(record.created_at).toLocaleString().toUpperCase()
         }));
 
         const csv_string = json2csv(report_data);
@@ -58,21 +51,29 @@ const getPackageReport = async (req, res, next) => {
 
 const getTaskReport = async (req, res, next) => {
     try {
-        const task = await Task.findOne({ task_id: req.query.task_id }).populate("packages").lean();
+        const task = await Task.findOne({ task_id: req.query.task_id })
+            .populate({
+                path: "packages",
+                populate: { path: "executive", select: "name username" }
+            })
+            .populate("created_by", "name username")
+            .lean();
 
-        const report_data = (task.packages || []).map(record => ({
+        const report_data = task.packages.map((record, i) => ({
             package_id: record.package_id,
+            type: record.type,
             courier: record.courier,
             channel: record.channel,
-            status: record.status,
-            outgoing_ts: new Date(record.outgoing.timestamp).toLocaleString(),
-            outgoing_executive: record.outgoing.executive,
-            outgoing_task: record.outgoing.task,
-            outgoing_remarks: record.outgoing.remarks,
-            incoming_ts: (record.incoming?.timestamp && new Date(record.incoming.timestamp).toLocaleString()) || "",
-            incoming_executive: record.incoming?.executive || "",
-            incoming_task: record.incoming?.task || "",
-            incoming_remarks: record.incoming?.remarks || ""
+            status: record.cancelled ? "cancel" : record.type === "incoming" ? "return" : "dispatch",
+            executive: `${record.executive?.name} (${record.executive?.username})`,
+            task_id: task.task_id,
+            remarks: record.remarks || "",
+            scanned_at: new Date(record.created_at).toLocaleString().toUpperCase(),
+            task_status: i === 0 ? task.is_open ? "open" : "closed" : "",
+            created_by: i === 0 ? `${task.created_by?.name} (${task.created_by?.username})` : "",
+            vehicle_no: i === 0 ? task.vehicle_no : "",
+            del_ex_name: i === 0 ? task.delex_name : "",
+            del_ex_ph: i === 0 ? task.delex_contact : ""
         }));
 
         const csv_string = json2csv(report_data);
@@ -86,6 +87,6 @@ const getTaskReport = async (req, res, next) => {
 }
 
 export {
-    getPackageReport,
-    getTaskReport
+    getTaskReport,
+    getPackageReport
 };
